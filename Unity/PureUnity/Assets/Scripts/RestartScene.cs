968a3bedeaf09fd75e52d6bf4ef52bd640a1a967
Michal <Michal@DESKTOP-JMT7K8K>
2020-01-10 11:40:52 +0100

    Added scene restart on R keynode, added Shortcuts button with tooltip.

---
+++ Unity/PureUnity/Assets/Scripts/RestartScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Restart scene when press 'R'.
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
