0e447a24736086aa5966dd2a57784dd67f18b301
Mar-zin <marcinanyway@gmail.com>
2020-01-17 08:19:05 +0100

    working on GUI

---
+++ Unity/PureUnity/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    [SerializeField] Canvas rules;
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void openCloseRules()
+    {
+        if (rules.enabled)
+        {
+            rules.enabled = false;
+        }
+        else
+        {
+            rules.enabled = true;
+        }
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Unity/PureUnity/Assets/Scripts/Settings.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    [SerializeField] Canvas settings;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EnableSettingsCanvas()
+    {
+        if (settings.enabled)
+        {
+            settings.enabled = false;
+        }
+        else
+        {
+            settings.enabled = true;
+        }
+    }
+}
